a1f64c48629e8c3c5ab4cc7520b2648885497a53
SeonWooJu <tjsdnwn21104@gmail.com>
2022-02-07 09:34:30 +0900

    2개 커밋

--- schedulingtasks/src/main/java/com/example/schedulingtasks/SchedulingtasksApplication.java
+++ schedulingtasks/src/main/java/com/example/schedulingtasks/SchedulingtasksApplication.java
@@ -2,12 +2,13 @@ package com.example.schedulingtasks;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling //예약 활성화
 public class SchedulingtasksApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SchedulingtasksApplication.class, args);
+		SpringApplication.run(SchedulingtasksApplication.class);
 	}
-
 }
